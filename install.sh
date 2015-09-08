240be198d611b3c1f0711235e57d68e0f4d818f1
Brunno dos Santos <squiter85@gmail.com>
2015-09-08 18:29:09 -0300

    Add jrnl_config file

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-files=( ackrc bashrc bash_profile emacs emacs.d config alias vim vimrc gitconfig gitignore_global git-templates tmux.conf zshrc bin gemrc pryrc )
+files=( ackrc bashrc bash_profile emacs emacs.d config alias vim vimrc gitconfig gitignore_global git-templates tmux.conf zshrc bin gemrc pryrc jrnl_config )
 
 for filename in ${files[@]}
 do
