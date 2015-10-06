8a9f455f95c4f925de0ef5eacef7e4c048d98db5
Brunno dos Santos <squiter85@gmail.com>
2015-10-06 17:11:58 -0300

    Removes `install.sh`!
    To install this repo you need to use: https://github.com/squiter/ansible-dev

--- install.sh
+++
@@ -1,9 +0,0 @@
-files=( ackrc bashrc bash_profile emacs emacs.d config alias vim vimrc gitconfig gitignore_global git-templates tmux.conf zshrc bin gemrc pryrc jrnl_config )
-
-for filename in ${files[@]}
-do
-[[ -s $HOME/.$filename ]] && rm -rf $HOME/.$filename
-  ln -sfi $PWD/$filename ~/.$filename
-done
-
-source ~/.bash_profile
