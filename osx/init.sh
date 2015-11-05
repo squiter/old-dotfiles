87ccdb6c1cc75e0856e27ea68547300ff6d7fc0b
Brunno dos Santos <squiter85@gmail.com>
2015-11-05 20:13:06 -0200

    Add hotcorner configuration for my OSX defaults

--- osx/init.sh
+++ osx/init.sh
@@ -10,5 +10,9 @@ defaults write com.apple.screencapture location -string "$HOME/Desktop"
 # Set Safari’s home page to `about:blank` for faster loading
 defaults write com.apple.Safari HomePage -string "about:blank"
 
+# Setup Bottom left screen corner → Start screen saver
+defaults write com.apple.dock wvous-bl-corner -int 5
+defaults write com.apple.dock wvous-bl-modifier -int 0
+
 # Xcode - Install command-line tools
 xcode-select --install || true
