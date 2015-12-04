bbb37a45c9df80f90552a80a1d6d8cf8b99137f0
Brunno dos Santos <squiter85@gmail.com>
2015-12-04 19:42:18 -0200

    Remove Emacs from my default dotfiles
    You can found my emacs files in http://github.com/squiter/emacs-dotfiles

--- emacs.d/conf/init-wakatime.el
+++
@@ -1,6 +0,0 @@
-(setq wakatime-api-key "84e8b465-1324-4e75-8109-6c6c4a1d3dee")
-(setq wakatime-cli-path "/usr/local/bin/wakatime")
-
-(global-wakatime-mode)
-
-(provide 'init-wakatime)
--- emacs.d/conf/init-yaml.el
+++
@@ -1,7 +0,0 @@
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
-(add-to-list 'auto-mode-alist '("\\.yml\\.example$" . yaml-mode))
-
-(add-hook 'yaml-mode-hook 'linum-mode)
-
-(provide 'init-yaml)
